526478bfcda92433165678c51a2fe682dd1125af
selcukaras <selcukalan.net@gmail.com>
2016-08-19 15:03:27 +0300

    -

---
+++ string1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Baskent = "Ankara";
+
+            String Sayi = "0 1 2 3 4 5 6 7 8 9";
+
+            String Tarih = "15.08.2016";
+
+            Console.WriteLine(Baskent.GetType());
+            Console.WriteLine(Sayi.GetType());
+            Console.WriteLine(Tarih.GetType());
+            Console.ReadKey();
+        }
+    }
+}
